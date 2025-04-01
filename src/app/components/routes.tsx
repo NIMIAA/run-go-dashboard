a1ef4523d085af282d5e03e7ab01c571e76aeb32
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-04-01 15:44:22 +0100

    feat:,impl layout

---
+++ src/app/components/routes.tsx
@@ -0,0 +1,41 @@
+import {
+  Squares2X2Icon,
+  UserIcon,
+  CurrencyDollarIcon,
+  Cog6ToothIcon,
+} from "@heroicons/react/24/outline";
+import {
+  Squares2X2Icon as SolidSquares2X2Icon,
+  UserIcon as SolidUserIcon,
+  CurrencyDollarIcon as SolidCurrencyDollar,
+  Cog6ToothIcon as SolidCog6ToothIcon,
+} from "@heroicons/react/24/solid";
+export const routes = [
+  {
+    path: "/dashboard",
+    label: "dashbaord",
+    defaultIcon: <Squares2X2Icon className="size-6" />,
+    activeicon: <SolidSquares2X2Icon className="size-6" />,
+  },
+
+  {
+    path: "/dashboard/profile",
+    label: "profile",
+    defaultIcon: <UserIcon className="size-6" />,
+    activeicon: <SolidUserIcon className="size-6" />,
+  },
+
+  {
+    path: "/dashboard/revenue",
+    label: "revenue",
+    defaultIcon: <CurrencyDollarIcon className="size-6" />,
+    activeicon: <SolidCurrencyDollar className="size-6" />,
+  },
+
+  {
+    path: "/dashboard/settings",
+    label: "settings",
+    defaultIcon: <Cog6ToothIcon className="size-6" />,
+    activeicon: <SolidCog6ToothIcon className="size-6" />,
+  },
+];
---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,62 @@
+"use client";
+import React, { ReactNode } from "react";
+import { routes } from "../components/routes";
+import Link from "next/link";
+import { ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/outline";
+
+interface Props {
+  children: ReactNode;
+}
+export default function Layout({ children }: Props) {
+  return (
+    <div className="grid grid-cols-12 h-screen w-full">
+      <nav className="col-span-2 px-4 pt-12 relative">
+        <ul className="flex flex-col gap-y-6">
+          {routes.map((route) => {
+            return (
+              <li key={route.label} className="">
+                <Link
+                  className="flex font-[family-name:var(--font-geist-sans)] items-center gap-x-2 prose-lg capitalize font-medium"
+                  href={route.path}
+                >
+                  {" "}
+                  {route.defaultIcon}
+                  {route.label}
+                </Link>
+              </li>
+            );
+          })}
+
+          <button className="flex font-[family-name:var(--font-geist-sans)] items-center gap-x-2 prose-lg capitalize font-medium position absolute bottom-10 w-full left-0 right-0 px-4">
+            <ArrowLeftStartOnRectangleIcon className="size-6" /> Logout
+          </button>
+        </ul>
+      </nav>
+      <main className="col-span-10 bg-white/50 h-screen overflow-x-hidden overflow-y-scroll">
+        <div role="header max-h-12 flex justify-between">
+          <h1 className="text-3xl font-semibold">Rungo</h1>
+          <label className="input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                stroke-linejoin="round"
+                stroke-linecap="round"
+                stroke-width="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" required placeholder="Search" />
+          </label>
+        </div>
+        {children}
+      </main>
+    </div>
+  );
+}
